fd3f51b9a993a16d6b10d4566cb7299ce220dbed
nkfr26 <n@rfu.jp>
2026-08-04 21:40:50 +0900

    Apply upgrade

--- packages/hono-jsx/src/useRemember.ts
+++ packages/hono-jsx/src/useRemember.ts
@@ -5,7 +5,7 @@ import { Dispatch, SetStateAction } from './types'
 export default function useRemember<State>(
   initialState: State,
   key?: string,
-  excludeKeysRef?: RefObject<string[] | null>,
+  excludeKeysRef?: RefObject<string[]>,
 ): [State, Dispatch<SetStateAction<State>>] {
   const [state, setState] = useState(() => {
     const restored = router.restore(key) as State
